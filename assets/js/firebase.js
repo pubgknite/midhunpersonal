3b953a624c0d0cbe81db7bcc2094cace8acda9fe
betterKNITE <pubgcp@gmail.com>
2022-03-14 23:23:13 +0300

    dfg

---
+++ assets/js/firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRAo-1K7XXbyXErB-I89x0zGHrqwcmqLg",
+    authDomain: "midhuncontrctform.firebaseapp.com",
+    databaseURL: "https://midhuncontrctform-default-rtdb.firebaseio.com",
+    projectId: "midhuncontrctform",
+    storageBucket: "midhuncontrctform.appspot.com",
+    messagingSenderId: "678916748581",
+    appId: "1:678916748581:web:8b952b84292efea7d3d6cc"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // reference your database
+ var contactForumDB firebase.database().ref("contractforum");
+
+ document.getElementById
+</script>
